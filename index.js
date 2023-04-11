3f2b9221d5ec28a0b31cabdcfcae6e7b8fcdcae7
Sumedh199 <129769187+sumedh500@users.noreply.github.com>
2023-04-12 00:55:14 +0530

    Input of Bill and cash Has added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const input=document.querySelector("#input-btn")
 const billAmount=document.querySelector("#input-amount")
+const cashGiven=document.querySelector("#cash-input")
 input.addEventListener("click",()=>{
-    console.log("hitt",billAmount.value)
+    console.log("Amount",billAmount.value)
+    console.log("Cash",cashGiven.value)
 
 })
