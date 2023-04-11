0d85386cb6f5195e5d8acee60e73f65f68e38f8b
Sumedh199 <129769187+sumedh500@users.noreply.github.com>
2023-04-12 00:53:10 +0530

    HTML ADD

---
+++ index.js
@@ -0,0 +1,6 @@
+const input=document.querySelector("#input-btn")
+const billAmount=document.querySelector("#input-amount")
+input.addEventListener("click",()=>{
+    console.log("hitt",billAmount.value)
+
+})
